021d1795fd8dedc8e77dc9c4210ef6ab9ee1ac49
F. Soldevila <soltfern@gmail.com>
2023-07-25 12:02:15 +0200

    Update sp_demo.py
    Following a pull request, changed a little bit how I add the noise on the signal so it feels more intuitive.
    
    Now, you can choose the SNR of the object to recover. The default SNR for the noise case is 20 dB

--- sp_demo.py
+++ sp_demo.py
@@ -3,16 +3,52 @@
 # Loads an object, generates the sensing patterns (Hadamard), 
 # does the measurements (with and without noise), and recovers the image.
 
-# %% import stuff
-# plot stuff
-from matplotlib import pyplot as plt 
-# generate hadamard matrices
-from scipy.linalg import hadamard    
-# working with images stuff
+#%% Import libraries
+from matplotlib import pyplot as plt
+import numpy as np
+# Generate hadamard matrices
+from scipy.linalg import hadamard
+# Working with images
 from skimage.transform import resize
 from PIL import Image
-# doing math stuff
-import numpy as np
+
+#%% Define useful functions
+def noisify(signal: np.ndarray, end_snr: float) -> (np.ndarray, np.ndarray):
+    '''
+    Add white gaussian noise to a signal so it ends with end_snr 
+    signal-to-noise ratio.
+    More info at: https://en.wikipedia.org/wiki/Signal-to-noise_ratio#Decibels
+    
+    Parameters
+    ----------
+    signal : np.ndarray
+        Input signal
+    end_snr : float
+        Desired SNR.
+    Returns
+    -------
+    noisy_signal : np.ndarray
+        Signal with added noise.
+    noise : np.ndarray
+        Noise added to the original signal.
+    '''
+    # Calculate signal average (~power)
+    signal_avg = np.mean(signal)
+    # Convert to dB
+    signal_avg_db = 10 * np.log10(signal_avg)
+    # Calculate noise power in dB, using objective SNR
+    # SNR = P_signal(in dB) - P_noise(in dB)
+    noise_avg_db = signal_avg_db - end_snr
+    # Convert to noise average (~power)
+    noise_avg = 10**(noise_avg_db/10)
+    # Build noise with desired power
+    noise = np.random.normal(0, np.sqrt(noise_avg), signal.size)
+    # Build additive noise (shift to positive-only values)
+    noise = noise + np.abs(np.min(noise))
+    # Add noise to signal
+    noisy_signal = signal + noise
+    
+    return noisy_signal, noise
 
 # %%  Load image & generate object
 # load image, convert to grayscale, store as array (matrix)
@@ -25,44 +61,51 @@ test_obj = resize(ground_obj,(px,px))
 #%% Generate measurement patterns using a Hadamard matrix. 
 ## Each row is a 2D pattern (after reshaping)
 H = hadamard(px**2)   # Complete Hadamard matrix (+1s and -1s)
-Hplus = (H+1)/2       # H+ Hadamard matrix (1s and 0s)
-Hminus = (1-H)/2      # H- Hadamard matrix (0s and 1s)
+Hplus = (H + 1) / 2       # H+ Hadamard matrix (1s and 0s)
+Hminus = (1 - H) / 2      # H- Hadamard matrix (0s and 1s)
 
 #%% Generate measurements simulating H+ and H- 
 ## (as if patterns were generated on a DMD)
-Mplus = Hplus@test_obj.flatten()    #Project H+ patterns, store intensity
-Mminus = Hminus@test_obj.flatten()  #Project H- patterns, store intensity
-M = Mplus - Mminus                  #Substract values (H+ - H-)
+Mplus = Hplus @ test_obj.flatten() # Project H+ patterns, store intensity
+Mminus = Hminus @ test_obj.flatten() # Project H- patterns, store intensity
+M = Mplus - Mminus # Substract values (H+ - H-)
 
+<<<<<<< Updated upstream
 # Generate measurements with noise
 noise_level = 2 #width (std) of the noise distribution (Gaussian)
 # Generate noises (random number using a Gaussian distribution)
 noise_plus = np.random.normal(np.mean(Mplus),noise_level,Mplus.size)
 noise_minus = np.random.normal(np.mean(Mminus),noise_level,Mminus.size)
+=======
+# Generate measurements from a noisy object
+desired_SNR = 20 # SNR desired for the object
+# Generate noisy object (adding white gaussian noise)
+test_obj_noisy, noise = noisify(test_obj.flatten(), desired_SNR)
+>>>>>>> Stashed changes
 # Add noise to the true coefficients
-Mplus_noise = Mplus + noise_plus        #Project H+
-Mminus_noise = Mminus + noise_minus     #Project H-
-M_noise = Mplus_noise - Mminus_noise    #Substract values (H+ - H-)
+Mplus_noisy = Hplus @ test_obj_noisy # Project H+
+Mminus_noisy = Hminus @ test_obj_noisy # Project H-
+M_noisy = Mplus_noisy - Mminus_noisy # Substract values (H+ - H-)
 
 #%% Recover objects
 # Inversion from measurements (solve the eq. systems with/without noise)
-recovery = np.linalg.solve(H,M)
-recovery_noise = np.linalg.solve(H,M_noise)
-# Reshape from vector into image           
-recovery = recovery.reshape((px,px));
-recovery_noise = recovery_noise.reshape((px,px));
+recovery = np.linalg.solve(H, M)
+recovery_noise = np.linalg.solve(H, M_noisy)
+# Reshape from vector into image
+recovery = recovery.reshape((px, px))
+recovery_noise = recovery_noise.reshape((px, px))
 
 #%% Show the results
 #Show recovery without noise
 plt.figure()
-plt.imshow(recovery,cmap='hot')
+plt.imshow(recovery, cmap = 'hot')
 plt.axis('on')
 plt.title('Recovered image without noise')
 plt.colorbar()
 plt.show()
 #Show recovery with noise
 plt.figure()
-plt.imshow(recovery_noise,cmap='hot')
+plt.imshow(recovery_noise, cmap = 'hot')
 plt.axis('on')
 plt.title('Recovered image with noise')
 plt.colorbar()
